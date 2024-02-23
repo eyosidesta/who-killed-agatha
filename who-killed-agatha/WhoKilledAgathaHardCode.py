0d6ff819cc5133c5646366f71a37e77149d0a608
eyosidesta <mamesmsdesta@gmail.com>
2024-02-22 22:16:05 -0500

    evaluation started

--- who-killed-agatha/WhoKilledAgathaHardCode.py
+++ who-killed-agatha/WhoKilledAgathaHardCode.py
@@ -12,8 +12,12 @@ class WhoKilledAgatha:
         # agatha hates herself (second index 1 which means agatha hates herself)
         # agata doesn't hate butler (third index 0 which means agatha doesn't hate butler)
         # agata hates charles (fourth/last index 1 which means agatha hates charles)
-        self.agatha_hate_array.push('Agatha', 1, 0, 1)
-        return self.agata_hate_array
+        self.agatha_hate_array.append('Agatha')        
+        self.agatha_hate_array.append(1)
+        self.agatha_hate_array.append(0)
+        self.agatha_hate_array.append(1)
+
+        return self.agatha_hate_array
     
     def butler_hates_who(self):
         # ** 'The butler hates everyone not richer than Aunt Agatha.
@@ -21,7 +25,10 @@ class WhoKilledAgatha:
         # butler doesn't hate himself (third index 0 which means butler doesn't hate himself)
         # ** 'The butler hates everyone whom Agatha hates'
         # butler hates charles (fourth/last index 1 which means butler hates charles)
-        self.butler_hate_array.push('Butler', 1, 0, 1)
+        self.butler_hate_array.append('Butler')
+        self.butler_hate_array.append(1)
+        self.butler_hate_array.append(0)
+        self.butler_hate_array.append(1)
         return self.butler_hate_array
     
 
@@ -30,20 +37,48 @@ class WhoKilledAgatha:
         # charles doesn't hate agatha (second index 1 which means charles doesn't hate agatha)
         # charles either hates or doesn't hate butler (i't not defined) (third index 0.5 which means it's unknown)
         # charles doesn't hate himself (fourth/last index 1 which means charles doesn't hate himself)
-        self.charles_hate_array.push('Charles', 0, 0.5, 0)
+        self.charles_hate_array.append('Charles')
+        self.charles_hate_array.append(0)
+        self.charles_hate_array.append(0.5)
+        self.charles_hate_array.append(0)
         return self.charles_hate_array
     
     def agatha_richer_than_who(self):
-        self.agatha_rich_array.push('Agatha', 0, 0, 0.5)
+        self.agatha_rich_array.append('Agatha')
+        self.agatha_rich_array.append(0)
+        self.agatha_rich_array.append(0)
+        self.agatha_rich_array.append(0.5)
         return self.agatha_rich_array
     
     def butler_richer_than_who(self):
         # ** 'butler hates everyone not richer than Aunt Agatha'
-        self.butler_richer_than_who.push('Butler', 1, 0, 0.5)
-        return self.butler_hate_array
+        self.butler_richer_than_who.append('Butler')
+        self.butler_richer_than_who.append(1)
+        self.butler_richer_than_who.append(0)
+        self.butler_richer_than_who.append(0.5)
+        return self.butler_rich_array
     
 
     def charles_richer_than_who(self):
-        self.charles_hate_array.push('Charles', 0.5, 0.5, 0)
-        return self.charles_hate_array
+        self.charles_hate_array.append('Charles')
+        self.charles_hate_array.append(0.5)
+        self.charles_hate_array.append(0.5)
+        self.charles_hate_array.append(0)
+        return self.charles_rich_array
     
+    def check_who_hates_agatha(self):
+        if self.agatha_hate_array[1] == 1:
+            self.hates_agatha.append('Agatha')
+        if self.butler_hate_array[1] == 1:
+            self.hates_agatha.append('Butler')
+        if self.charles_hate_array[1] == 1:
+            self.hates_agatha.append('Charles')
+
+        return self.hates_agatha
+    
+    def check_not_richer_than_agatha(self):
+        pass
+
+find_killer = WhoKilledAgatha()
+find_killer.agatha_hates_who()
+print("the value is: ", find_killer.agatha_hate_array)
