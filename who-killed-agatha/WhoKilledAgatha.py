9193b9998df7b0ab800e1667c945ceae2e6c69f5
eyosidesta <mamesmsdesta@gmail.com>
2024-02-25 02:15:17 -0500

    variables added

--- who-killed-agatha/WhoKilledAgatha.py
+++ who-killed-agatha/WhoKilledAgatha.py
@@ -4,4 +4,13 @@ class AgathaSolver:
     def __init__(self):
         self.problem = Problem()
 
-   
+    def add_variables(self):
+        people = ['Agatha', 'Butler', 'Charles']
+        hates_values = [0, 1]
+        richer_values = [0, 1, 'not_defined']
+
+        # Adding variables to the problem
+        self.problem.addVariables(people, hates_values)
+        self.problem.addVariables([f"{person}_richer" for person in people], richer_values)
+
+    
